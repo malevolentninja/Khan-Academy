e836d25b9f2c1d01afe91209772ea71a7ad455b9
Olivia Boertje <malevolentninja@gmail.com>
2016-11-20 16:16:18 +0000

    Update and rename Challenge: TODO List to Challenge: TODO List.sql

--- SQL/Challenge: TODO List.sql
+++ SQL/Challenge: TODO List.sql
@@ -1,7 +1,9 @@
 CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
 INSERT INTO todo_list VALUES (1, "Wash the dishes", 15);
-INSERT INTO todo_list VALUES (2, "vacuuming", 20);
-INSERT INTO todo_list VALUES (3, "Learn some stuff on KA", 30);
+INSERT INTO todo_list VALUES (2, "Hoover", 20);
+INSERT INTO todo_list VALUES (3, "Learn some computer languages", 60);
 INSERT INTO todo_list VALUES (4, "Wash hair", 20);
+INSERT INTO todo_list VALUES (5, "Cleaning bathroom", 40);
+
 
 SELECT SUM(minutes) FROM todo_list;
