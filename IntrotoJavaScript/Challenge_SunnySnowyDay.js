66e4a18d256dc46e34ee02192391927424bca119
Olivia Boertje <malevolentninja@gmail.com>
2017-04-27 20:14:33 +0100

    Create Challenge_SunnySnowyDay.js

---
+++ IntrotoJavaScript/Challenge_SunnySnowyDay.js
@@ -0,0 +1,18 @@
+// snowman on a sunny day using fill (), background(), ellipse() and rect() commands
+
+
+background(22, 209, 222);
+// The ground
+fill(0, 255, 89);
+rect(0, 300, 400, 100);  
+
+// The sun
+fill(222, 255, 36);
+ellipse(80, 64, 100, 100);  
+
+// The snowman
+fill(255, 255, 255);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+
