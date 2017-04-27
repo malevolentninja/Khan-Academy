e267ba261a8d15e1fba91a1ad819e80d7160c4b1
Olivia Boertje <malevolentninja@gmail.com>
2017-04-27 20:54:22 +0100

    Create Project_WhatIsForDinner.js

---
+++ IntrotoJavaScript/Project_WhatIsForDinner.js
@@ -0,0 +1,34 @@
+// What is For Dinner? 
+// Create a meal  - I decided a slice of pizza and a few peas
+
+
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+// Peas
+fill(28, 176, 63);
+strokeWeight(2);
+ellipse(200, 301, 10, 10); // pea six
+ellipse(190, 290, 10, 10); // pea one
+ellipse(180, 290, 10, 10); // pea two
+ellipse(170, 301, 10, 10); // pea three
+ellipse(180, 301,10, 10); // pea four
+ellipse(190, 301, 10, 10); // pea five
+ellipse(185, 280, 10, 10); // pea seven
+
+// Pizza Base
+fill (255, 183, 0);
+strokeWeight(5);
+triangle(109, 92, 209, 300, 200, 80); 
+
+// Pizza Crust
+fill(224, 127, 9);
+strokeWeight(5);
+rect(100, 74, 105, 20, 120);
+
+// Pepperoni
+fill (219, 27, 27);
+ellipse(150, 140, 20, 20); // pepperoni one
+ellipse(180, 160, 20, 20); // pepperoni two
+ellipse(180, 202, 20, 20); // pepperoni three
