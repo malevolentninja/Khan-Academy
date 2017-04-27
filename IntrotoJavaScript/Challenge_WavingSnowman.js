3cb91536a03ac2c75ac5729b1186ab26df78ee90
Olivia Boertje <malevolentninja@gmail.com>
2017-04-27 19:55:22 +0100

    Update Challenge_WavingSnowman.js

--- IntrotoJavaScript/Challenge_WavingSnowman.js
+++ IntrotoJavaScript/Challenge_WavingSnowman.js
@@ -1,6 +1,8 @@
-// Make a waving snowman using the ellipse() and rect() commands
+// Make a waving snowman using the ellipse(),  rect() and line() commands
 
-rect(1, 370, 398, 30);
-ellipse(200, 300, 150, 150);
-ellipse(200, 200, 100, 100);
-ellipse(200, 120, 75, 75);
+rect(1, 370, 398, 30); // block snowman is standing on
+ellipse(200, 300, 150, 150); // bottom circle
+ellipse(200, 200, 100, 100); //middle circle
+ellipse(200, 120, 75, 75); // top circle
+line(160,200,10,100); // left arm
+line(240,200,350,90); // right arm
