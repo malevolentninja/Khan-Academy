c49ccf6b0809c5468e708cb834b2f7ae5eefbcc8
Olivia Boertje <malevolentninja@gmail.com>
2017-04-27 19:47:16 +0100

    Create Challenge_SimpleSnowman.js

---
+++ IntrotoJavaScript/Challenge_SimpleSnowman.js
@@ -0,0 +1,9 @@
+// Challenge : Draw a snowman using ellipse() command. 
+
+
+
+ellipse(200, 300, 150, 150); // bottom circle:
+
+ellipse(200, 205, 100,100);// middle circle
+
+ellipse(200, 130, 70, 70); // top circle
